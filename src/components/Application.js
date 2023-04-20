318905e7096cb5957dc1e589c2ccfa0ec125b99b
RorySchof <roryschofield@gmail.com>
2023-04-19 21:04:54 -0400

    Built a button with classnames

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 import "components/Application.scss";
@@ -6,11 +7,26 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          {/* Add some content here */}
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
       </section>
     </main>
   );
 }
+
+
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,71 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   const buttonClass = classNames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
+ 
+
+// export default function Button(props) {
+//    return <button>{props.children}</button>;
+//  }
+
+//  export default function Button(props) {
+//    let buttonClass = "button";
+ 
+//    if (props.confirm) {
+//      buttonClass += " button--confirm";
+//    }
+ 
+//    return <button className={buttonClass}>{props.children}</button>;
+//  }
+
+//  export default function Button(props) {
+//    let buttonClass = "button";
+ 
+//    if (props.confirm) {
+//      buttonClass += " button--confirm";
+//    }
+ 
+//    if (props.danger) {
+//      buttonClass += " button--danger";
+//    }
+ 
+//    return <button className={buttonClass}>{props.children}</button>;
+//  }
+
+//  export default function Button(props) {
+//    let buttonClass = "button";
+ 
+//    if (props.confirm) {
+//      buttonClass += " button--confirm";
+//    }
+ 
+//    if (props.danger) {
+//      buttonClass += " button--danger";
+//    }
+ 
+//    return (
+//      <button
+//        className={buttonClass}
+//        onClick={props.onClick}
+//        disabled={props.disabled}
+//      >
+//        {props.children}
+//      </button>
+//    );
+//  }
